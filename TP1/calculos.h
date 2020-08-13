20daed4bc5b6389a99957da560374e55fcb71611
martin-pedraza <56758686+martin-pedraza@users.noreply.github.com>
2020-08-13 19:27:36 -0300

    Actualizado

---
+++ TP1/calculos.h
@@ -0,0 +1,50 @@
+#ifndef CALCULOS_H_INCLUDED
+#define CALCULOS_H_INCLUDED
+
+
+
+#endif // CALCULOS_H_INCLUDED
+
+int sumar(int op1, int op2);
+/** \brief Sumar ambos parametros y obtener un resultado
+ *
+ * \param primer numero a sumar
+ * \param segundo numero a sumar
+ * \return resultado de la suma
+ *
+ */
+
+int restar(int op1, int op2);
+/** \brief Restar el segundo parametro al primero y obtener el resultado
+ *
+ * \param primer numero
+ * \param numero que restara al primero
+ * \return resultado de la resta
+ *
+ */
+
+float dividir(int op1, int op2);
+/** \brief Dividir el primer parametro por el segundo y obtener un resultado
+ *
+ * \param primer numero
+ * \param numero que dividira al primero
+ * \return resultado de la division
+ *
+ */
+
+int multiplicar(int op1, int op2);
+/** \brief Multiplicar los parametros y obtener un resultado
+ *
+ * \param primer numero
+ * \param segundo numero
+ * \return resultado de la multiplicacion
+ *
+ */
+
+int factorial(int op);
+/** \brief Realizar el factorial del parametro y obtener el resultado
+ *
+ * \param numero para hacer el factorial
+ * \return resultado del factorial
+ *
+ */
